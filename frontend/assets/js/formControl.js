5232110001365ea007689af7d1133a30a0bced80
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-02-22 18:34:52 +0100

    Beautifying code & web look-and-feel (live coding)

---
+++ frontend/assets/js/formControl.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    /**
+     * Function for showing password
+     */
+    if ($("#showPassword").length) {
+        $('#showPassword').click(function () {
+            let passwordField = $("#password");
+            if (passwordField.attr("type") === "password") {
+                $('i').removeClass('fa-eye-slash').addClass('fa-eye');
+                passwordField.attr("type", "text");
+            } else {
+                $('i').removeClass('fa-eye').addClass('fa-eye-slash');
+                passwordField.attr("type", "password");
+            }
+        });
+    }
+
+
+    /**
+     * Function for showing datepicker in registration pages
+     */
+    if ($("#birthdate").length) {
+        $("#birthdate").datepicker({
+            clearBtn: true,
+            format: "dd/mm/yyyy",
+            language: "es",
+            todayHighlight: true,
+            weekStart: 1
+        });
+    }
+
+    /**
+     * Function for showing dan selector when it's necessary
+     */
+    if ($("#beltSelector").length) {
+        $("#beltSelector").change(function () {
+            $(this).siblings("#danField").css("display", $(this).val() === "N" ? "inherit" : "none");
+            console.log($(this).val());
+        });
+    }
+});
