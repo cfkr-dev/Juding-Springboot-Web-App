b6ae4eb97dbba92aea6ae8c0e802a8c0c59db333
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-01 12:42:21 +0100

    Trial commit.

--- backend/src/main/java/es/dawgroup2/juding/JudingApplication.java
+++ backend/src/main/java/es/dawgroup2/juding/JudingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JudingApplication {
-
+    // Main method of application
     public static void main(String[] args) {
         SpringApplication.run(JudingApplication.class, args);
     }
