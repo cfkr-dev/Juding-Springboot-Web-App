6d1988e1801623bac3ecb51569512f71bda14c84
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-01 12:38:21 +0100

    First version of backend (only scaffolding).

---
+++ backend/src/main/java/es/dawgroup2/juding/JudingApplication.java
@@ -0,0 +1,13 @@
+package es.dawgroup2.juding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JudingApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/es/dawgroup2/juding/JudingApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dawgroup2.juding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
