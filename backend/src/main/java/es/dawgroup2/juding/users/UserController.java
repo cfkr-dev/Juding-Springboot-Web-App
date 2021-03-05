38658212e94b816176e66d57a541ca8cc79822f9
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-05 21:46:32 +0100

    Dynamic view of user control panel (for admins).
    The new view (accesible at /admin/user/list) allows admins to get a list of registered users (with some buttons -not working in this moment- for editing and deleting them).

--- backend/src/main/java/es/dawgroup2/juding/users/UserController.java
+++ backend/src/main/java/es/dawgroup2/juding/users/UserController.java
@@ -1,14 +1,28 @@
 package es.dawgroup2.juding.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class UserController {
 
-    @GetMapping("/myProfile")
-    public String viewProfile(Model model){
-        return "user/viewProfile";
+    @Autowired
+    UserRepository userRepository;
+
+    String licenseId = "1234567890";
+
+    /*
+     * ADMIN PAGES - LIST AND EDITION OF USERS
+     */
+    @GetMapping("/admin/user/list")
+    public String userList(Model model){
+        List<User> userList = userRepository.findAll();
+        model.addAttribute("userList", userList);
+        return "/admin/user/list";
     }
 }
