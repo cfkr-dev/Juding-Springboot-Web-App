75e1a3831b7b8aa5abed248da465ce57a7548c5e
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-05 13:30:05 +0100

    Style changes.

--- frontend/assets/js/competitorsList.js
+++ frontend/assets/js/competitorsList.js
@@ -3,6 +3,3 @@ $("#modalProfile").on("show.bs.modal", function (evt) {
     $("#modalProfile #modalOpenedTitle").html($(evt.relatedTarget).parent().siblings(".nameModal").html());
     $("#modalProfile #modalOpenedBody").html($(evt.relatedTarget).parent().siblings(".viewMore").html());
 });
-$(document).ready(function () {
-    $(".rtRow:even").css("background-color", "rgba(255, 204, 204, 0.5)"); // filas impares
-});
