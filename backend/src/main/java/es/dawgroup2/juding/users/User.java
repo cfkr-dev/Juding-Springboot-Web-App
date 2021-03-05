da181e2462892daf3ca2249a653471ec447f98fa
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-05 19:23:21 +0100

    Model completed (all attributes and methods).

--- backend/src/main/java/es/dawgroup2/juding/users/User.java
+++ backend/src/main/java/es/dawgroup2/juding/users/User.java
@@ -10,64 +10,207 @@ public class User {
     @Id
     private String licenseId;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private String name;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private String surname;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private String email;
-    
+
     private int phone;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private char gender;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private Date birthDate;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private int dni;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private int gym;
 
-    protected User() { }
+    @Column(nullable = false)
+    private int weight;
+
+    @Column(nullable = false)
+    private String belt;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String securityQuestion;
+
+    @Column(nullable = false)
+    private String securityAnswer;
+
+    @Column(nullable = false)
+    private int role;
+
+    private int refereeRange;
+
+    protected User() {
+    }
+
+    public User(String licenseId, String name, String surname, String email, int phone, char gender, Date birthDate, int dni, int gym, int weight, String belt, String nickname, String password, String securityQuestion, String securityAnswer, int role) {
+        this.licenseId = licenseId;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.dni = dni;
+        this.gym = gym;
+        this.weight = weight;
+        this.belt = belt;
+        this.nickname = nickname;
+        this.password = password;
+        this.securityQuestion = securityQuestion;
+        this.securityAnswer = securityAnswer;
+        this.role = role;
+    }
 
     public String getLicenseId() {
         return licenseId;
+    }
+
+    public void setLicenseId(String licenseId) {
+        this.licenseId = licenseId;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getSurname() {
         return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public int getPhone() {
         return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
     }
 
     public char getGender() {
         return gender;
     }
 
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
     public Date getBirthDate() {
         return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
     }
 
     public int getDni() {
         return dni;
     }
 
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
     public int getGym() {
         return gym;
+    }
+
+    public void setGym(int gym) {
+        this.gym = gym;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getBelt() {
+        return belt;
+    }
+
+    public void setBelt(String belt) {
+        this.belt = belt;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public String getSecurityAnswer() {
+        return securityAnswer;
+    }
+
+    public void setSecurityAnswer(String securityAnswer) {
+        this.securityAnswer = securityAnswer;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public int getRefereeRange() {
+        return refereeRange;
+    }
+
+    public void setRefereeRange(int refereeRange) {
+        this.refereeRange = refereeRange;
     }
 }
