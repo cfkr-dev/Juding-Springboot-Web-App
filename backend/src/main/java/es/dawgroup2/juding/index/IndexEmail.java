8a636c7b9e2017dbf2fd54df10b05d1b363d15b2
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-08 18:10:09 +0100

    Creating "model" object.
    This object allows Controller to receive post values from form.

---
+++ backend/src/main/java/es/dawgroup2/juding/index/IndexEmail.java
@@ -0,0 +1,29 @@
+package es.dawgroup2.juding.index;
+
+public class IndexEmail {
+
+    private final String name, email, subject, message;
+
+    public IndexEmail(String name, String email, String subject, String message) {
+        this.name = name;
+        this.email = email;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
