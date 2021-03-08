493e93e472457e6d242194c4f2018a9f5f483f0e
Ismaelgzse <ismaelgzse@gmail.com>
2021-03-08 12:52:17 +0100

    Creating competition repository

---
+++ backend/src/main/java/es/dawgroup2/juding/competitions/CompetitionRepository.java
@@ -0,0 +1,7 @@
+package es.dawgroup2.juding.competitions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompetitionRepository extends JpaRepository<Competition,String > {
+
+}
