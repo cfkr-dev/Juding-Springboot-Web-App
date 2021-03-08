ed74eb39811b1b08df844d91fe9c67ce249b7c19
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-08 18:15:34 +0100

    Controller with mail sending (static version).
    This version of controller is done using common post sending and a controller that redirects to a successful page (index with a request parameter) when email is properly sent.

--- backend/src/main/java/es/dawgroup2/juding/index/IndexController.java
+++ backend/src/main/java/es/dawgroup2/juding/index/IndexController.java
@@ -3,20 +3,31 @@ package es.dawgroup2.juding.index;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.Optional;
 
-@RestController
+@Controller
 public class IndexController {
 
     @Autowired
     private JavaMailSender emailSender;
 
+    @GetMapping("/")
+    public String index(Model model, @RequestParam Optional<String> status) {
+        if (status.isPresent())
+            model.addAttribute("status", (status.get().equals("success")) ? status : false);
+        return "index";
+    }
+
     @PostMapping("/index-email")
-    public boolean sendEmail(IndexEmail indexEmail) {
+    public String sendEmail(IndexEmail indexEmail) {
         MimeMessage mimeMessage = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
         String htmlMsg = "<h2>Mensaje por formulario web</h2>" +
@@ -33,8 +44,8 @@ public class IndexController {
             helper.setReplyTo(indexEmail.getName() + "<" + indexEmail.getEmail() + ">");
             emailSender.send(mimeMessage);
         } catch (MessagingException e) {
-            return false;
+            e.printStackTrace();
         }
-        return true;
+        return "/?status=success#contact";
     }
 }
