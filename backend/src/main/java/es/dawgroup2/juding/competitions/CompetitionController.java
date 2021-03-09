982ec1402552b9dc2da0c829f30c1606f84d3b39
Ismaelgzse <ismaelgzse@gmail.com>
2021-03-09 21:34:26 +0100

    Added the feature to update the parameters of a competition

--- backend/src/main/java/es/dawgroup2/juding/competitions/CompetitionController.java
+++ backend/src/main/java/es/dawgroup2/juding/competitions/CompetitionController.java
@@ -28,28 +28,9 @@ public class CompetitionController {
         Competition competition = competitionService.findById(idCompetition);
         model.addAttribute("competition", competition);
         return "/admin/competition/edit";
-
-    }
-    @GetMapping("/admin/competition/deleteCompetition/{idCompetition}")
-    public String showCompetitionToDelete(Model model, @PathVariable String idCompetition) {
-        Competition competition = competitionService.findById(idCompetition);
-        model.addAttribute("competition", competition);
-        return "/admin/competition/{idCompetition}";
     }
 
-   @DeleteMapping("/admin/competition/deleteCompetition/{idCompetition}")
-    public String deleteCompetition(@PathVariable String idCompetition) {
-        Competition competition = competitionService.findById(idCompetition);
-        competitionService.deleteById(idCompetition);
-        return "/admin/competition/list";
-    }
-
-    @PostMapping("/admin/competition/newCompetition")
-    public String addingCompetition(Competition competition){
-        competitionService.add(competition);
-        return "/admin/competition/list";
-    }
-    @PutMapping("admin/competition/edit/{idCompetition}")
+    @PostMapping("/admin/competition/edit")
     public String updatingCompetitionInfo(@RequestParam String idCompetition, @RequestParam String shortName, @RequestParam String additionalInfo, @RequestParam int minWeight, @RequestParam int maxWeight, @RequestParam Timestamp startDate, @RequestParam Timestamp endDate, @RequestParam String referee, @RequestParam int refereeStatus) {
         Competition competition = competitionService.findById(idCompetition);
         competition.setIdCompetition(Integer.parseInt(idCompetition));
@@ -66,8 +47,35 @@ public class CompetitionController {
 
     }
 
-    /*@PutMapping("/admin/competition/edit/{idCompetition}")
-    public String updatingCompetitionInfo(Model model,@RequestParam Competition competition){
+    @GetMapping("/admin/competition/deleteCompetition/{idCompetition}")
+    public String showCompetitionToDelete(Model model, @PathVariable String idCompetition) {
+        Competition competition = competitionService.findById(idCompetition);
+        model.addAttribute("competition", competition);
+        return "/admin/competition/{idCompetition}";
+    }
+
+    @DeleteMapping("/admin/competition/deleteCompetition/{idCompetition}")
+    public String deleteCompetition(@PathVariable String idCompetition) {
+        Competition competition = competitionService.findById(idCompetition);
+        competitionService.deleteById(idCompetition);
+        return "/admin/competition/list";
+    }
+    @GetMapping("/admin/competition/newCompetition")
+    public String showFormNewCompetition(@PathVariable String idCompetition, Model model) {
+        Competition competition = competitionService.findById(idCompetition);
+        model.addAttribute("competition", competition);
+        return "redirect:/admin/competition/list";
+    }
+
+    @PostMapping("/admin/competition/newCompetition")
+    public String addACompetition(Competition competition) {
+        competitionService.add(competition);
+        return "redirect:/admin/competition/list";
+    }
+
+
+    /*@PostMapping("/admin/competition/edit")
+    public String updatingCompetitionInfo(@RequestParam Competition competition){
         competitionService.updatingInfoCompetition(competition);
         return "redirect:/admin/competition/list";
     }*/
