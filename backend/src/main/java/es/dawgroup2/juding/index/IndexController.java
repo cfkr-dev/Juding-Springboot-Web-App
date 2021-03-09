a13ac76db9d18c91bc65592e36d9eccbf92f6440
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-09 18:08:00 +0100

    Dynamic version - bug fixes.

--- backend/src/main/java/es/dawgroup2/juding/index/IndexController.java
+++ backend/src/main/java/es/dawgroup2/juding/index/IndexController.java
@@ -1,51 +1,14 @@
 package es.dawgroup2.juding.index;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    @Autowired
-    private JavaMailSender emailSender;
-
     @GetMapping("/")
-    public String index(Model model, @RequestParam Optional<String> status) {
-        if (status.isPresent())
-            model.addAttribute("status", (status.get().equals("success")) ? status : false);
+    public String index(){
         return "index";
-    }
-
-    @PostMapping("/index-email")
-    public String sendEmail(IndexEmail indexEmail) {
-        MimeMessage mimeMessage = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
-        String htmlMsg = "<h2>Mensaje por formulario web</h2>" +
-                "<ul>" +
-                "   <li>Remitente: " + indexEmail.getName() + " (" + indexEmail.getEmail() + ").</li>" +
-                "   <li>Asunto: " + indexEmail.getSubject() + ".</li>" +
-                "   <li>Mensaje:</li>" +
-                "</ul>" + indexEmail.getMessage().replaceAll("\r|\n|\r\n", "<br>");
-        try {
-            helper.setText(htmlMsg, true); // Use this or above line.
-            helper.setTo("juding.noreply@gmail.com");
-            helper.setSubject("Mensaje página web: " + indexEmail.getSubject());
-            helper.setFrom(indexEmail.getName() + "<" + indexEmail.getEmail() + ">");
-            helper.setReplyTo(indexEmail.getName() + "<" + indexEmail.getEmail() + ">");
-            emailSender.send(mimeMessage);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-        return "/?status=success#contact";
     }
 }
--- backend/src/main/java/es/dawgroup2/juding/indexEmail/IndexEmail.java
+++ backend/src/main/java/es/dawgroup2/juding/indexEmail/IndexEmail.java
@@ -1,4 +1,4 @@
-package es.dawgroup2.juding.index;
+package es.dawgroup2.juding.indexEmail;
 
 public class IndexEmail {
 
