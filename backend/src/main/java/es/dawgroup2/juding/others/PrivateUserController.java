affb80de6be776d0c48ae058651887197a2a14a0
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-12 00:23:09 +0100

    Edit profile (view and form saving) now working.

--- backend/src/main/java/es/dawgroup2/juding/others/PrivateUserController.java
+++ backend/src/main/java/es/dawgroup2/juding/others/PrivateUserController.java
@@ -1,7 +1,9 @@
 package es.dawgroup2.juding.others;
 
+import es.dawgroup2.juding.belts.BeltService;
 import es.dawgroup2.juding.users.User;
 import es.dawgroup2.juding.users.UserService;
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -11,8 +13,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.mail.Multipart;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.sql.SQLException;
 
 @Controller
@@ -23,6 +30,9 @@ public class PrivateUserController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    BeltService beltService;
 
     @GetMapping("/myHome")
     public String myHome(Model model, HttpServletResponse response) {
@@ -70,9 +80,36 @@ public class PrivateUserController {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         } else {
             model.addAttribute("user", currentUser)
-                    .addAttribute("isCompetitor", currentUser.isCompetitor());
+                    .addAttribute("isCompetitor", currentUser.isCompetitor())
+                    .addAttribute("beltSelector", beltService.getSelectFieldWithCurrentValue(currentUser.getBelt().name()));
         }
         return "/myProfile/edit";
     }
 
+    @PostMapping("/myProfile/edit")
+    public String editingUser(Model model, @RequestParam String licenseId,
+                              @RequestParam String beltSelector,
+                              @RequestParam(required = false) String gym,
+                              @RequestParam(required = false) Integer weight,
+                              @RequestParam(required = false) Integer refereeRange,
+                              @RequestParam String nick,
+                              @RequestParam int phone,
+                              @RequestParam String email,
+                              MultipartFile image) throws IOException {
+        User user = userService.getUserOrNull(licenseId);
+        // Common fields
+        user.setBelt(beltService.findBeltById(beltSelector)).setNickname(nick).setPhone(phone).setEmail(email);
+        // Role-based fields
+        if (user.getRole() == 1) {
+            user.setGym(gym).setWeight(weight);
+        } else if (user.getRole() == 2) {
+            user.setRefereeRange(refereeRange);
+        }
+        // Changing image
+        if (!image.isEmpty()) {
+            user.setProfileImage(BlobProxy.generateProxy(image.getInputStream(), image.getSize()));
+        }
+        userService.save(user);
+        return "redirect:/myProfile";
+    }
 }
