0dd730520d6ab186b43423a69e0b4bf8c38d2708
C0nf1cker <56350482+C0nf1cker@users.noreply.github.com>
2021-03-12 04:08:18 +0100

    Added post controller.

--- backend/src/main/java/es/dawgroup2/juding/posts/PostController.java
+++ backend/src/main/java/es/dawgroup2/juding/posts/PostController.java
@@ -1,5 +1,6 @@
 package es.dawgroup2.juding.posts;
 
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -7,36 +8,67 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Controller
 public class PostController {
 
     @Autowired
-    private PostRepository postRepository;
+    PostService postService;
 
     @GetMapping("/admin/post/list")
-    public String postList(Model model){
-        List<Post> postList = postRepository.findAll();
+    public String postList(Model model) {
+        List<Post> postList = postService.findAll();
         model.addAttribute("postList", postList);
         return "/admin/post/list";
     }
 
+    @GetMapping("/index")
+    public String indexPosts(Model model) {
+        List<Post> postList = postService.findAll();
+        model.addAttribute("postList", postList);
+        return "/index";
+    }
+
+    @GetMapping("/news/{id}")
+    public String NewsPost(Model model, @PathVariable String id) {
+        Post post = postService.findById(id);
+        List<Post> postList = postService.findAll();
+        postList.remove(post);
+        model.addAttribute("post", post);
+        model.addAttribute("postList", postList);
+        return "/news";
+    }
+
+    @GetMapping("/admin/post/list/{id}")
+    public String postModal(Model model, @PathVariable String id) {
+        Post post = postService.findById(id);
+        model.addAttribute("post", post);
+        return "/admin/post/list";
+    }
+
     @GetMapping("/admin/post/edit/{id}")
-    public String postEdit(Model model, @PathVariable String id){
-        Post post = postRepository.findById(Integer.parseInt(id)).orElseThrow();
+    public String postEdit(Model model, @PathVariable String id) {
+        Post post = postService.findById(id);
         model.addAttribute("post", post);
         return "/admin/post/edit";
     }
 
-    @GetMapping("/admin/post/edit/image/{id}")
+    @GetMapping("/admin/post/createNew")
+    public String newPost(Model model) {
+        return "/admin/post/createNew";
+    }
+
+    @GetMapping("/image/{id}")
     public ResponseEntity<Object> downloadImage(@PathVariable String id)
             throws SQLException {
-        Post post = postRepository.findById(Integer.parseInt(id)).orElseThrow();
+        Post post = postService.findById(id);
         if (post.getImageFile() != null) {
             Resource file = new InputStreamResource(
                     post.getImageFile().getBinaryStream());
@@ -47,5 +79,44 @@ public class PostController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping("/admin/post/edit/modify")
+    public String updatingPost(@RequestParam String id, @RequestParam String title, @RequestParam MultipartFile image, @RequestParam String body) throws IOException, SQLException {
+        Post post = postService.findById(id);
+        post.setTitle(title);
+        post.setBody(body);
+        post.setTimestamp(new Timestamp(System.currentTimeMillis()));
+        if (!image.isEmpty()) {
+            post.setImageFile(BlobProxy.generateProxy(image.getInputStream(), image.getSize()));
+        } else {
+            Post dbpost = postService.findById(id);
+            if (dbpost.getImageFile() != null) {
+                post.setImageFile(BlobProxy.generateProxy(dbpost.getImageFile().getBinaryStream(),
+                        dbpost.getImageFile().length()));
+            }
+        }
+        postService.updatingInfoPost(post);
+        return "redirect:/admin/post/list";
+    }
+
+    @PostMapping("/admin/post/createNew")
+    public String addNewPost(@RequestParam String title, @RequestParam MultipartFile image, @RequestParam String body) throws IOException, SQLException {
+        Post post = new Post();
+        post.setAuthor("1234567890"); //How to reference this?
+        post.setTitle(title);
+        post.setBody(body);
+        post.setTimestamp(new Timestamp(System.currentTimeMillis()));
+        if (!image.isEmpty()) {
+            post.setImageFile(BlobProxy.generateProxy(image.getInputStream(), image.getSize()));
+        }
+        postService.add(post);
+        return "redirect:/admin/post/list";
+    }
+
+    @GetMapping("/admin/post/delete/{id}")
+    public String deletePost(@PathVariable String id) {
+        postService.deleteById(id);
+        return "redirect:/admin/post/list";
     }
 }
