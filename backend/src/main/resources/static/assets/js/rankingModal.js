6337501b3ced6c74d330fb17cf18d95da4eb8005
C0nf1cker <56350482+C0nf1cker@users.noreply.github.com>
2021-03-12 04:05:42 +0100

    Some JS changes about modal views

--- backend/src/main/resources/static/assets/js/rankingModal.js
+++
@@ -1,5 +0,0 @@
-$("#modalProfile").on("show.bs.modal", function (evt) {
-    console.log(evt.relatedTarget);
-    $("#modalProfile #modalOpenedTitle").html($(evt.relatedTarget).parent().siblings(".nameModal").html());
-    $("#modalProfile #modalOpenedBody").html($(evt.relatedTarget).parent().siblings(".viewMore").html());
-});
