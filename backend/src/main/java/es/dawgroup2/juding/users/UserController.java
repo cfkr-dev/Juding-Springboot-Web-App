2a99a00b18442cf8355584063751938330324913
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-14 00:56:33 +0100

    Admitting and rejecting referee applications.

--- backend/src/main/java/es/dawgroup2/juding/users/UserController.java
+++ backend/src/main/java/es/dawgroup2/juding/users/UserController.java
@@ -1,15 +1,22 @@
 package es.dawgroup2.juding.users;
 
 import es.dawgroup2.juding.belts.BeltService;
+import es.dawgroup2.juding.users.refereeRange.RefereeRange;
 import es.dawgroup2.juding.users.refereeRange.RefereeRangeService;
 import es.dawgroup2.juding.users.roles.Role;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -23,6 +30,9 @@ public class UserController {
 
     @Autowired
     RefereeRangeService refereeRangeService;
+
+    @Autowired
+    private JavaMailSender emailSender;
 
     /*
      * VIEWS
@@ -60,6 +70,34 @@ public class UserController {
     public String savingUser(User user) {
         userService.save(user);
         return "redirect:/admin/user/list/";
+    }
+
+    @GetMapping("/admin/user/admitReferee/{licenseId}")
+    public String admitReferee(@PathVariable String licenseId) {
+        User user = userService.getUserOrNull(licenseId);
+        userService.save(user.setRefereeRange(RefereeRange.E));
+
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+        String htmlMsg = "Estimado <strong>" + user.getName() + " " + user.getSurname() + "</strong>.<br>" +
+                "<br>" +
+                "Le anunciamos que ha sido <strong>ADMITIDO</strong> como árbitro en la Federación de Judo de la Comunidad de Madrid.<br>" +
+                "Ya puede acceder a la aplicación oficial haciendo uso de las credenciales que proporcionó al registrarse.<br>" +
+                "<br>" +
+                "Reciba un cordial saludo.<br>" +
+                "<br>" +
+                "Federación de Judo de la Comunidad de Madrid.";
+        try {
+            helper.setText(htmlMsg, true); // Use this or above line.
+            helper.setTo(user.getName() + " " + user.getSurname() + " <" + user.getEmail() + ">");
+            helper.setSubject("Solicitud de admisión de árbitros");
+            helper.setFrom("Federación de Judo CAM <juding.noreply@gmail.com>");
+            emailSender.send(mimeMessage);
+        } catch (Exception e) {
+            return "redirect:/error/500";
+        }
+
+        return "redirect:/admin/user/list/referees";
     }
 
 
