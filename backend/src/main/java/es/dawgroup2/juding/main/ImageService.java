10d6169237f8fb397d75da42e7220859294fac37
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-15 00:41:19 +0100

    Improving image service modularity.

--- backend/src/main/java/es/dawgroup2/juding/main/ImageService.java
+++ backend/src/main/java/es/dawgroup2/juding/main/ImageService.java
@@ -27,25 +27,20 @@ public class ImageService {
     }
 
     public Blob uploadProfileImageWithBufferedImage(BufferedImage bi) throws IOException {
-        // Get image dimensions
         int height = bi.getHeight();
         int width = bi.getWidth();
 
-        // The image is already a square
         if (height != width) {
-            // Compute the size of the square
             int squareSize = (Math.min(height, width));
 
-            // Coordinates of the image's middle
             int xc = width / 2;
             int yc = height / 2;
 
-            // Crop
             BufferedImage croppedImage = bi.getSubimage(
-                    xc - (squareSize / 2), // x coordinate of the upper-left corner
-                    yc - (squareSize / 2), // y coordinate of the upper-left corner
-                    squareSize,            // widht
-                    squareSize             // height
+                    xc - (squareSize / 2),
+                    yc - (squareSize / 2),
+                    squareSize,
+                    squareSize
             );
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             ImageIO.write(croppedImage, "jpg", baos);
