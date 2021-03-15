61e7710add1d74fee49db83e7aeb2728498cf912
Ismaelgzse <ismaelgzse@gmail.com>
2021-03-15 21:44:59 +0100

    Password encoder works
    Added pom dependencies
    Added the folder of security
    Modified UserDataLoader

---
+++ backend/src/main/java/es/dawgroup2/juding/security/RepositoryUserDetailsService.java
@@ -0,0 +1,36 @@
+package es.dawgroup2.juding.security;
+
+import es.dawgroup2.juding.users.User;
+import es.dawgroup2.juding.users.UserRepository;
+import es.dawgroup2.juding.users.roles.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RepositoryUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String nick) throws UsernameNotFoundException {
+        User user = userRepository.findByNickname(nick)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+        List<GrantedAuthority> roles = new ArrayList<>();
+        for (Role role : user.getRoles()) {
+            roles.add(new SimpleGrantedAuthority("ROLE_" + role));
+        }
+        return new org.springframework
+                .security
+                .core
+                .userdetails
+                .User(user.getNickname(),user.getPassword(),roles);
+    }
+}
