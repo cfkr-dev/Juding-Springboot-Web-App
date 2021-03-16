ed94bb3694c128f48e9e41cadd9fb7e994155e0a
C0nf1cker <56350482+C0nf1cker@users.noreply.github.com>
2021-03-16 12:09:03 +0100

    Added new functionalities about forms validation.

--- backend/src/main/resources/static/assets/js/formControl.js
+++ backend/src/main/resources/static/assets/js/formControl.js
@@ -30,6 +30,16 @@ if ($("#showPassword").length) {
 }
 
 
+/*
+ * CONTROLLING SUBMISSION WITH FORBIDDEN VALUES
+ */
+
+let forbiddenLicenseId;
+let forbiddenDni;
+let forbiddenNickname;
+let forbiddenMaxWeight;
+let forbiddenEndDate;
+
 $("#licenseId").on("blur", function () {
     if ($("#licenseId")[0].checkValidity()) {
         $.ajax({
@@ -94,4 +104,40 @@ $("#dni").on("blur", function () {
     } else {
         $("#dniBanner").remove();
     }
+});
+
+$("#maxWeight").on("blur", function (){
+    console.log($("#maxWeight").parent().siblings(".minWeight").children("#minWeight").val());
+    if ($("#maxWeight")[0].checkValidity()) {
+        $("#maxWeightBanner").remove();
+        if ($("#maxWeight").val() <= $("#maxWeight").parent().siblings(".minWeight").children("#minWeight").val()) {
+            $("#maxWeight").after('<div class="alert alert-danger mt-1" id="maxWeightBanner">No se puede introducir un peso máximo menor que el peso mínimo</div>');
+            forbiddenMaxWeight = true;
+        } else {
+            forbiddenMaxWeight = false;
+        }
+    } else{
+        $("#maxWeightBanner").remove();
+    }
+})
+
+$("#endDate").on("blur", function (){
+    if ($("#endDate")[0].checkValidity()) {
+        $("#endDateBanner").remove();
+        if ($("#endDate").val() <= $("#endDate").parent().siblings(".startDate").children("#startDate").val()) {
+            $("#endDate").after('<div class="alert alert-danger mt-1" id="endDateBanner">No se puede introducir una fecha de fin anterior a la fecha de inicio</div>');
+            forbiddenEndDate = true;
+        } else {
+            forbiddenEndDate = false;
+        }
+    } else{
+        $("#endDateBanner").remove();
+    }
+})
+
+$(".juding-form").on("submit", function(evt){
+    evt.preventDefault();
+    console.log(forbiddenDni || forbiddenNickname || forbiddenLicenseId || forbiddenMaxWeight || forbiddenEndDate)
+    if (forbiddenDni || forbiddenNickname || forbiddenLicenseId || forbiddenMaxWeight || forbiddenEndDate)
+        $(this).off("submit");
 });
