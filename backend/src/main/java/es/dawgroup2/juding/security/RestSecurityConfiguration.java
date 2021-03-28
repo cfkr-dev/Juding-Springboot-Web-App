e49343f556c110f1696b8095a1e5b44fb13b3cc8
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-03-29 01:24:15 +0200

    Fixing token generator error.

--- backend/src/main/java/es/dawgroup2/juding/security/RestSecurityConfiguration.java
+++ backend/src/main/java/es/dawgroup2/juding/security/RestSecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -46,6 +45,8 @@ public class RestSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/api/**");
+
 		// UserController
 		http.authorizeRequests().antMatchers("/api/admin/**").hasRole(Role.A.name());
 
