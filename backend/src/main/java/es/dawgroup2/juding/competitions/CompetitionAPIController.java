b363116debeba3faf2b6085bde9538f3b408c945
Ismaelgzse <ismaelgzse@gmail.com>
2021-03-30 20:58:28 +0200

    First version of the admin competition Rest controller

---
+++ backend/src/main/java/es/dawgroup2/juding/competitions/CompetitionAPIController.java
@@ -0,0 +1,51 @@
+package es.dawgroup2.juding.competitions;
+
+import es.dawgroup2.juding.fight.FightService;
+import es.dawgroup2.juding.main.DateService;
+import es.dawgroup2.juding.main.HeaderInflater;
+import es.dawgroup2.juding.users.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/competition")
+public class CompetitionAPIController {
+    @Autowired
+    HeaderInflater headerInflater;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    CompetitionService competitionService;
+
+    @Autowired
+    FightService fightService;
+
+    @Autowired
+    DateService dateService;
+
+    @GetMapping("/{idCompetition}")
+    public Competition showCompetition(@PathVariable String idCompetition, HttpServletRequest request) {
+        Competition competition = competitionService.findById(Integer.parseInt(idCompetition));
+        return competition;
+    }
+
+    @PutMapping("/{idCompetition}/join")
+    public ResponseEntity<Competition> joinCompetition(@PathVariable String idCompetition, HttpServletRequest request) {
+        Competition competition = competitionService.findById(Integer.parseInt(idCompetition));
+        competitionService.joinCompetition(competition, userService.findByNickname(request.getUserPrincipal().getName()));
+        competitionService.add(competition);
+        if (competition!= null){
+            return ResponseEntity.ok(competition);
+        }
+        else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
+
