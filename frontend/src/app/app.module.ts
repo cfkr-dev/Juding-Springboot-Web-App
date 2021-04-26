8ee76649b935be5d199e883a44038682d6fd8a71
C0nf1cker <56350482+C0nf1cker@users.noreply.github.com>
2021-04-26 13:28:54 +0200

    Adding login functionalities and fixing some errors

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {LoginComponent} from './login/login.component';
+import { CompetitorListComponent } from './admin/user/components/competitor-list.component';
 import { routing } from './app.routing';
 import {HeaderComponent} from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, CompetitorListComponent, LoginComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule, routing],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -1,5 +1,9 @@
 import {RouterModule} from '@angular/router';
+import {CompetitorListComponent} from './admin/user/components/competitor-list.component';
 
-const appRoutes = [];
+const appRoutes = [
+    {path: 'competitors', component: CompetitorListComponent},
+    {path: '', redirectTo: 'competitors', pathMatch: 'full'}
+];
 
 export const routing = RouterModule.forRoot(appRoutes);
