0c845f852afe0e9697db151a54f24bad306f8056
jolutoher18 <61881281+jolutoher18@users.noreply.github.com>
2021-04-26 12:35:17 +0200

    Demo models

---
+++ frontend/src/app/user/user.model.ts
@@ -0,0 +1,12 @@
+import {belt} from "./belt.model";
+import {gender} from "./gender.model";
+
+export class User {
+    nickname: "Jose";
+    name:"Kk";
+    surname:"KKKKKK";
+    weight:80;
+    descripcion: "Esto es la descriocion";
+    belt: belt;
+    gender: gender;
+}
