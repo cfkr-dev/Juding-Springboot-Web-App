7013dd7bf354f74577509ace7208373b66fa9183
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-04-28 13:53:13 +0200

    New modular header in Angular frontend
    This new modular header adapts its contents according to current page.

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {LoggedInUserService} from '../logged-in-user.service';
+import {UserInterface} from '../user/user.interface';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['../../assets/css/header.css']
+  styleUrls: ['../../assets/vendor/font-awesome/css/all.css', '../../assets/css/header.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  currentUser: UserInterface;
 
-  ngOnInit(): void {
+  // 0 -> Default (logged-in user)
+  // 1 -> Index page
+  // 2 -> Only show "return to index" button
+  @Input() mainPage: number;
+
+  headerType2 = [
+    '/login',
+    '/termsAndConditionsOfUse',
+    '/cookiePolicy'
+  ];
+
+  constructor(private loggedUser: LoggedInUserService, public router: Router) {
+    if (this.mainPage === undefined) {
+      this.mainPage = 0;
+    }
+    if (this.headerType2.includes(this.router.url)){
+      this.mainPage = 2;
+    }
   }
 
+  ngOnInit(): void {
+    this.loggedUser.getLoggedUser().subscribe(
+      (currentUser => this.currentUser = currentUser)
+    );
+  }
 }
