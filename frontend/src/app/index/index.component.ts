42b57f99dda1334812f5403133e0b999746b5a12
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2021-04-28 13:49:58 +0200

    Index page working with Angular

--- frontend/src/app/index/index.component.ts
+++ frontend/src/app/index/index.component.ts
@@ -1,51 +1,65 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 import {PostInterface} from '../post/post.interface';
+import {IndexService} from './index.service';
 
 interface Email {
-    name: string;
-    email: string;
-    subject: string;
-    message: string;
-    infoMessage: string;
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
 }
 
-interface EmailResponse{
-    class: string;
-    message: string;
+interface Posts{
+  list: PostInterface[];
+  hasNext: boolean;
+  isLoading: boolean;
+  page: number;
 }
 
 @Component({
-    selector: 'app-index',
-    templateUrl: './index.component.html',
-    styleUrls: ['../../assets/vendor/font-awesome/css/all.css', '../../assets/css/style.css', '../../assets/css/header.css', '../../assets/css/bootstrapAccomodations.css']
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['../../assets/vendor/font-awesome/css/all.css', '../../assets/css/style.css', '../../assets/css/header.css', '../../assets/css/bootstrapAccomodations.css'],
+  providers: [IndexService]
 })
 export class IndexComponent implements OnInit {
 
-    emailValues: Email;
-    emailResponse: EmailResponse;
+  emailValues: Email;
+  // 0 -> invisible
+  // 1 -> sending
+  // 2 -> success
+  // 3 -> error
+  emailResponse: number;
+  posts: Posts;
 
-    constructor(private http: HttpClient) {
-        this.emailValues = {email: '', message: '', name: '', subject: '', infoMessage: ''};
-        this.emailResponse = {class: '', message: ''};
-    }
+  constructor(private indexService: IndexService) {
+    this.emailValues = {email: '', message: '', name: '', subject: ''};
+    this.emailResponse = 0;
+    this.posts = {hasNext: true, isLoading: false, list: [], page: 0};
+  }
 
-    page = 0;
-    size = 0;
-    postList: PostInterface[];
+  ngOnInit(): void {
+    this.getMorePosts();
+  }
 
-    ngOnInit(): void {
-        // this.http.get('/api/posts?page=' + this.page + '&size=' + this.size).subscribe(
-        //     response => {for (let post of response){ this.postList.concat(post); }}
-        // );
-    }
+  getMorePosts(): void{
+    this.posts.isLoading = true;
+    this.indexService.getPosts(this.posts.page).subscribe(
+      (response) => {
+        response.content.map(post => this.posts.list.push(post));
+        this.posts.hasNext = !response.last;
+        this.posts.isLoading = false;
+        this.posts.page += 1;
+      }
+    );
+  }
 
-    email(event: Event): void {
-        event.preventDefault();
-        this.emailResponse = {class: 'info', message: 'Enviando...'};
-        this.http.get('/api/index-email?name=' + this.emailValues.name + '&email=' + this.emailValues.email + '&subject=' + this.emailValues.subject + '&message=' + this.emailValues.message).subscribe(
-            (response => this.emailResponse = {class: 'success', message: 'Correo enviado correctamente.'}),
-            (error => this.emailResponse = {class: 'danger', message: 'Algo no funcionó bien. Inténtelo de nuevo.'})
-        );
-    }
+  email(event: Event): void {
+    event.preventDefault();
+    this.emailResponse = 1;
+    this.indexService.sendEmail(this.emailValues).subscribe(
+      (response => this.emailResponse = 2),
+      (error => this.emailResponse = 3)
+    );
+  }
 }
---
+++ frontend/src/app/index/index.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {PostInterface} from '../post/post.interface';
+
+@Injectable()
+export class IndexService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  sendEmail(emailValues): Observable<any> {
+    return this.http.get('/api/index-email?name=' + emailValues.name + '&email=' + emailValues.email + '&subject=' + emailValues.subject + '&message=' + emailValues.message);
+  }
+
+  getPosts(page: number): Observable<any>{
+    return this.http.get('/api/posts?page=' + page + '&size=3') as Observable<PostInterface[]>;
+  }
+}
