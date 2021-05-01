38cd5d9d97c18bd1d2420352568755a865fad38d
C0nf1cker <56350482+C0nf1cker@users.noreply.github.com>
2021-05-01 03:03:50 +0200

    Bug fixing and added form validation.

--- frontend/src/app/admin/user/components/competitor-edit-component.ts
+++ frontend/src/app/admin/user/components/competitor-edit-component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CompetitorService} from '../services/competitor.service';
 import {User} from '../../../user/user.model';
-import {ErrorHandlerService} from '../services/error-handler.service';
 
 @Component({
     selector: 'app-competitor-edit-component',
@@ -19,28 +18,34 @@ export class CompetitorEditComponent implements OnInit {
     user: User;
     errorOnLoadUserData: boolean;
     validationError: boolean;
+    loadingContent: boolean;
+    submitted: boolean;
 
-    constructor(private router: Router, activatedRoute: ActivatedRoute, private competitorService: CompetitorService, private errorHandlerService: ErrorHandlerService) {
+    constructor(private router: Router, activatedRoute: ActivatedRoute, private competitorService: CompetitorService) {
+        this.loadingContent = true;
         const licenseId = activatedRoute.snapshot.params['licenseId'];
         competitorService.getCompetitor(licenseId).subscribe(
             user => {
+                this.loadingContent = false;
                 this.user = user;
                 this.errorOnLoadUserData = false;
             },
             error => {
                 this.errorOnLoadUserData = true;
-                this.errorHandlerService.handleError(error);
+                this.router.navigate(['500']);
             }
         );
         this.validationError = false;
+        this.submitted = false;
     }
 
     ngOnInit(): void {
     }
 
     modifyCompetitor(user) {
+        this.submitted = true;
         this.competitorService.updateCompetitor(user).subscribe(
-            modifiedUser => this.router.navigate(['/competitors']),
+            modifiedUser => this.router.navigate(['/admin/competitors']),
             // error => this.validationError = this.errorHandlerService.handleError(error)
         );
     }
--- frontend/src/app/admin/user/components/referee-edit.component.ts
+++ frontend/src/app/admin/user/components/referee-edit.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from '../../../user/user.model';
 import {ActivatedRoute, Router} from '@angular/router';
-import {ErrorHandlerService} from '../services/error-handler.service';
 import {RefereeService} from '../services/referee.service';
+import {getTransformedQueryCallExpr} from '@angular/core/schematics/migrations/static-queries/transform';
 
 @Component({
     selector: 'app-referee-edit',
@@ -18,27 +18,33 @@ export class RefereeEditComponent implements OnInit {
 
     user: User;
     errorOnLoadUserData: boolean;
+    loadingContent: boolean;
+    submitted: boolean;
 
-    constructor(private router: Router, activatedRoute: ActivatedRoute, private refereeService: RefereeService, private errorHandlerService: ErrorHandlerService) {
+    constructor(private router: Router, activatedRoute: ActivatedRoute, private refereeService: RefereeService) {
+        this.loadingContent = true;
         const licenseId = activatedRoute.snapshot.params['licenseId'];
         refereeService.getReferee(licenseId).subscribe(
             user => {
+                this.loadingContent = false;
                 this.user = user;
                 this.errorOnLoadUserData = false;
             },
             error => {
                 this.errorOnLoadUserData = true;
-                this.errorHandlerService.handleError(error);
+                this.router.navigate(['500']);
             }
         );
+        this.submitted = false;
     }
 
     ngOnInit(): void {
     }
 
     modifyReferee(user) {
+        this.submitted = true;
         this.refereeService.updateReferee(user).subscribe(
-            modifiedUser => this.router.navigate(['/referees']),
+            modifiedUser => this.router.navigate(['/admin/referees']),
             // error => this.validationError = this.errorHandlerService.handleError(error)
         );
     }
